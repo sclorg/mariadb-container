082e4a3fe64f380ce2f441034bb3edefcb29c6cc
Honza Horak <hhorak@redhat.com>
2019-02-01 16:26:44 +0100

    Fix openshift tests to run correctly locally and in remote cluster as well

--- test/test-lib-mysql.sh
+++ test/test-lib-mysql.sh
@@ -114,12 +114,13 @@ function test_mysql_s2i() {
 function test_mariadb_integration() {
   local image_name=$1
   local VERSION=$2
-  local import_image=${3:-}
-  local service_name=${image_name##*/}
+  local import_image=$3
+  local service_name=${import_image##*/}
+  ct_os_template_exists mariadb-ephemeral && t=mariadb-ephemeral || t=mariadb-persistent
   ct_os_test_template_app_func "${image_name}" \
-                               "mariadb-persistent" \
+                               "${t}" \
                                "${service_name}" \
-                               "ct_os_check_cmd_internal '${image_name}' '${service_name}' \"echo 'SELECT 42 as testval\g' | mysql --connect-timeout=15 -h <IP> testdb -utestu -ptestp\" '^42' 120" \
+                               "ct_os_check_cmd_internal '${import_image}' '${service_name}' \"echo 'SELECT 42 as testval\g' | mysql --connect-timeout=15 -h <IP> testdb -utestu -ptestp\" '^42' 120" \
                                "-p MARIADB_VERSION=${VERSION} \
                                 -p DATABASE_SERVICE_NAME="${service_name}-testing" \
                                 -p MYSQL_USER=testu \
@@ -127,3 +128,4 @@ function test_mariadb_integration() {
                                 -p MYSQL_DATABASE=testdb" "" "${import_image}"
 }
 
+# vim: set tabstop=2:shiftwidth=2:expandtab:

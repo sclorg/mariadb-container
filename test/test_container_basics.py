e114f3fb0d0c46dd8273258d4a94029c86355df0
Petr "Stone" Hracek <phracek@redhat.com>
2026-01-13 13:09:57 +0100

    Fix calling get_cip_cid directly from 'container-ci-suite'.
    Update also description for better reading what each test does
    in case it is huge
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_container_basics.py
+++ test/test_container_basics.py
@@ -27,27 +27,27 @@ class TestMariaDBBasicsContainer:
     """
 
     def setup_method(self):
+        """
+        Setup the test environment.
+        """
         self.app_image = build_s2i_app(app_path=VARS.TEST_DIR / "test-app")
         self.app_image.set_new_db_type(db_type="mariadb")
 
     def teardown_method(self):
+        """
+        Teardown the test environment.
+        """
         self.app_image.cleanup()
-
-    def get_cip_cid(self, cid_file_name):
-        """
-        Get the IP and container ID from the cid file name.
-        """
-        cip = self.app_image.get_cip(cid_file_name=cid_file_name)
-        assert cip
-        cid = self.app_image.get_cid(cid_file_name=cid_file_name)
-        assert cid
-        return cip, cid
 
     def test_s2i_usage(self):
         """
-        Test container creation fails with invalid combinations of arguments.
+        Test if the MariaDB container works properly
+        Steps are:
+        1. Test if the container creation fails with invalid combinations of arguments
+        2. Test if the container creation succeeds with valid combinations of arguments
+        3. Test if the database connection works
         """
-        cid_config_build = "s2i_config_build"
+        cid_config_build = "s2i_usage_build"
         self.app_image.assert_container_creation_fails(
             cid_file_name=cid_config_build,
             command="",
@@ -68,7 +68,8 @@ class TestMariaDBBasicsContainer:
                 "-e MYSQL_OPERATIONS_PASSWORD=operations_user",
             ],
         )
-        cip, cid = self.get_cip_cid(cid_file_name=cid_config_build)
+        cip, cid = self.app_image.get_cip_cid(cid_file_name=cid_config_build)
+        assert cip, cid
         assert self.app_image.test_db_connection(
             container_ip=cip, username="operations_user", password="operations_user"
         )
@@ -76,17 +77,24 @@ class TestMariaDBBasicsContainer:
 
     def test_s2i_usage_with_mount(self):
         """
-        Test container creation fails with invalid combinations of arguments.
+        Test if the MariaDB container works properly with mounted application directory.
+        Steps are:
+        1. Copy the test-app directory to a temporary directory
+        2. Create a container with the mounted application directory
+        3. Test if the container creation succeeds with valid combinations of arguments
+        4. Test if the database connection works with the operations user
+        5. Stop the container
+        6. Remove the temporary directory
         """
         data_dir = tempfile.mkdtemp(prefix="/tmp/mariadb-test_data")
         shutil.copytree(VARS.TEST_DIR / "test-app", f"{data_dir}/test-app")
         assert ContainerTestLibUtils.commands_to_run(
             commands_to_run=[
-                f"chown -R 27:27 {data_dir}/test-app",
+                f"chown -R mysql:mysql {data_dir}/test-app",
             ]
         )
         cid_s2i_test_mount = "s2i_test_mount"
-        self.app_image.create_container(
+        assert self.app_image.create_container(
             cid_file_name=cid_s2i_test_mount,
             container_args=[
                 "-e MYSQL_USER=config_test_user",
@@ -97,7 +105,8 @@ class TestMariaDBBasicsContainer:
                 f"-v {data_dir}/test-app:/opt/app-root/src/:z",
             ],
         )
-        cip, cid = self.get_cip_cid(cid_file_name=cid_s2i_test_mount)
+        cip, cid = self.app_image.get_cip_cid(cid_file_name=cid_s2i_test_mount)
+        assert cip, cid
         assert self.app_image.test_db_connection(
             container_ip=cip,
             username="operations_user",

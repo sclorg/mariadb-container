8e7510d3917c5f9b21a303952e4aa08c401ae9ed
Petr "Stone" Hracek <phracek@redhat.com>
2026-01-13 13:05:08 +0100

    Fix conftest.py so SSL_OPTION and PREVIOUS_VERSION is defined in VARS variable directly
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/conftest.py
+++ test/conftest.py
@@ -14,6 +14,13 @@ TAGS = {
     "rhel9": "-el9",
     "rhel10": "-el10",
 }
+
+MARIADB_PREVIOUS_VERSIONS = {
+    "10.3": "10.2",
+    "10.5": "10.3",
+    "10.11": "10.5",
+    "11.8": "10.11",
+}
 TEST_DIR = Path(__file__).parent.absolute()
 Vars = namedtuple(
     "Vars",
@@ -26,18 +33,19 @@ Vars = namedtuple(
         "TEST_APP",
         "VERY_LONG_DB_NAME",
         "VERY_LONG_USER_NAME",
-        "DB_NAME",
+        "SSL_OPTION",
+        "PREVIOUS_VERSION",
     ],
 )
 VERSION = os.getenv("VERSION")
 OS = os.getenv("TARGET").lower()
 TEST_APP = TEST_DIR / "test-app"
-VERY_LONG_DB_NAME = "very_long_database_name_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-VERY_LONG_USER_NAME = "very_long_user_name_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+VERY_LONG_DB_NAME = "very_long_database_name_" + "x" * 20
+VERY_LONG_USER_NAME = "very_long_user_name_" + "x" * 20
 # MariaDB 11.3+ enforces stricter SSL/TLS verification (certificate & hostname checks),
 # so tests may require this option for compatibility.
 # https://mariadb.org/mission-impossible-zero-configuration-ssl/
-DB_NAME = "--disable-ssl-verify-server-cert db" if VERSION == "11.8" else "db"
+SSL_OPTION = "--disable-ssl-verify-server-cert" if VERSION == "11.8" else ""
 VARS = Vars(
     OS=OS,
     VERSION=VERSION,
@@ -47,15 +55,6 @@ VARS = Vars(
     TEST_APP=TEST_APP,
     VERY_LONG_DB_NAME=VERY_LONG_DB_NAME,
     VERY_LONG_USER_NAME=VERY_LONG_USER_NAME,
-    DB_NAME=DB_NAME,
+    SSL_OPTION=SSL_OPTION,  # used for tests that require SSL verification to be disabled
+    PREVIOUS_VERSION=MARIADB_PREVIOUS_VERSIONS.get(VERSION),
 )
-
-
-def get_previous_major_version():
-    version_dict = {
-        "10.3": "10.2",
-        "10.5": "10.3",
-        "10.11": "10.5",
-        "11.8": "10.11",
-    }
-    return version_dict.get(VARS.VERSION)

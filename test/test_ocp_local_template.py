5990f63c4560ebbeee795da7263792ab339c6f81
Nikola Davidova <ndavidov@redhat.com>
2026-02-09 14:15:59 +0100

    Update tests to exclude 11.8 until released

--- test/test_ocp_local_template.py
+++ test/test_ocp_local_template.py
@@ -37,6 +37,8 @@ class TestMariaDBDeployTemplate:
         ]
     )
     def test_python_template_inside_cluster(self, template):
+        if VERSION == "11.8":
+             pytest.skip("Version is not released yet, so skipping for now.")
         short_version = VERSION.replace(".", "")
         assert self.oc_api.deploy_template_with_image(
             image_name=IMAGE_NAME,
